6ae9ed273f47edcbe0785f586f671428ef313046
ねんねこ <nennneko5787@gmail.com>
2024-10-05 23:19:28 +0900

    ai.py を更新

--- cogs/ai.py
+++ cogs/ai.py
@@ -10,25 +10,28 @@ class AIChatCog(commands.Cog):
         self.client = AsyncOpenAI(base_url="https://api.voids.top/v1", api_key="39")
         
     @commands.command()
-    async def aichat(self, ctx: commands.Context, prompt: str):
+    async def aichat(self, ctx: commands.Context, *, prompt: str):
         message = await ctx.reply("生成中")
-        stream = await self.client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
-            ],
-            model="gemini-1.5-pro-exp-0827",
-            stream=True,
-        )
-        async for chunk in stream:
-            try:
-                content = content + chunk.choices[0].delta.content or ""
-                message = await message.edit(content)
-            except:
-                pass
-            await asyncio.sleep(0.3)
+        try:
+            stream = await self.client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    }
+                ],
+                model="gemini-1.5-pro-exp-0827",
+                stream=True,
+            )
+            async for chunk in stream:
+                try:
+                    content = content + chunk.choices[0].delta.content or ""
+                    message = await message.edit(content)
+                except:
+                    pass
+                await asyncio.sleep(0.3)
+        except Exception as e:
+            await message.edit(str(e))
             
 async def setup(bot: commands.Bot):
     await bot.add_cog(AIChatCog(bot))
