cdc9c12f4778bfa62cb7b2609524297c28fbcb8e
ねんねこ <nennneko5787@gmail.com>
2024-10-05 23:02:42 +0900

    ai.py を更新

--- cogs/ai.py
+++ cogs/ai.py
@@ -5,7 +5,7 @@ from openai import AsyncOpenAI
 class AIChatCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.client = AsyncOpenAI(base_url="https://api.voids.top/v1", api_key="")
+        self.client = AsyncOpenAI(base_url="https://api.voids.top/v1", api_key="39")
         
     @commands.command()
     async def aichat(self, ctx: commands.Context, prompt: str):
